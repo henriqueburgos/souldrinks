105d71dc23a353ce721435378d315263dc02f854
Ana Paula <anapaulacff@outlook.com>
2022-05-19 21:40:11 -0300

    criacao da pagina produto e adicao de rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { CadastroComponent } from './components/cadastro/cadastro.component';
 import { FeedbackComponent } from './components/feedback/feedback.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { ProdutoComponent } from './components/produto/produto.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'feedback', component: FeedbackComponent },
-  { path: 'cadastro', component: CadastroComponent }
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'cadastro', component: ProdutoComponent}
 ];
 
 
