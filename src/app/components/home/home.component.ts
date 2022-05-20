64250a4b0a5ab2d955da3f3d65cecc025016bf4d
Taisaferreiraflavio <taisaferreiraflavio@gmail.com>
2022-05-20 11:31:52 -0300

    implementação de 1 cartão de produto na home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,6 +10,9 @@ export class HomeComponent implements OnInit {
   
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
+  imagem: string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaFcUST9q5zJ4_pYp-kP1Dq78vDXndT0T-Ig&usqp=CAU"
+
+
   constructor() { }
 
   ngOnInit(): void {
